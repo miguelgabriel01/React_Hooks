4a7c5dd13946a1cba151531764d1ede362b8f4cb
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-03-19 18:36:16 -0300

    finalização da aula sobre usestate1 origamid

--- usestate1/src/App.js
+++ usestate1/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ButtonModal from './ButtonModal';
+import Modal from './Modal';
 
 function App() {
+const [modal,setModal] = React.useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<h1>UseState 1</h1>
+<Modal modal={modal} setModal={setModal}/>
+<ButtonModal setModal={setModal}/>
+  </>
   );
+
 }
 
 export default App;
