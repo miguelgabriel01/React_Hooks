d6fca1421f7ca50bed6886564a59be571fe4b39d
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-03-28 13:26:22 -0300

    exemplo de varios useeffect com multiplos efeitos

--- useeffect/src/App.js
+++ useeffect/src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
 const App = () => {
   const [contar, setContar] = React.useState(0);
-  const [dados, setDados] = React.useState(null);
+  const [modal, setModal] = React.useState(false);
 
   React.useEffect(() => {
-    // se o fetch estivesse fora do useEffect, toda vez que o componente
-    // fosse atualizado, o mesmo seria executado
-    fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
-      .then((response) => response.json())
-      .then((json) => setDados(json));
-  }, []);
+    document.title = 'Total ' + contar;
+  }, [contar]);
+
+  React.useEffect(() => {
+    setContar(0);
+  }, [modal]);
 
   return (
     <div>
-      {dados && (
-        <div>
-          <h1>{dados.nome}</h1>
-          <p>R$ {dados.preco * contar}</p>
-        </div>
-      )}
+      {modal && <p>Meu Modal</p>}
+      <button onClick={() => setModal(!modal)}>Modal</button>
+      <hr />
       <button onClick={() => setContar(contar + 1)}>{contar}</button>
     </div>
   );
 };
-
+ 
 export default App;
